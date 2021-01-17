88591fbc117081864e5036dafcb08d3458d3fa95
jack_young <jackjordanyoung@gmail.com>
2021-01-17 17:48:25 +0000

    Fix for lines with slope greater than 1

--- draw.lua
+++ draw.lua
@@ -5,8 +5,15 @@ function line(x1, y1, x2, y2)
   gpu.setBackground(0xffffff)
   local m = (y2-y1)/(x2-x1)
   local c = (y1-m*x1)
+  local y = 0
   for x = x1, x2, 1 do
-    gpu.set(x, m*x+c, " ")
+    if (m*x+c - y > 1) do
+      for iy = y, m*x+c, 1 do
+        gpu.set(x, iy, " ")
+      end
+    end
+    y = m*x+c
+    gpu.set(x, y, " ")
   end
   gpu.setBackground(0x000000)
 end
