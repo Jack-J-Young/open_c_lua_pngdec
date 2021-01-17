5a145e76e7b10f8182409179c5bd09c0be531e49
jack_young <jackjordanyoung@gmail.com>
2021-01-17 17:37:10 +0000

    add draw to updater

--- updater.lua
+++ updater.lua
@@ -1,1 +1,2 @@
 shell.execute("/bin/wget.lua -f 'https://raw.githubusercontent.com/Jack-J-Young/open_c_blackjack/main/blackjack.lua' /home/scripts/blackjack.lua")
+shell.execute("/bin/wget.lua -f 'https://raw.githubusercontent.com/Jack-J-Young/open_c_blackjack/main/draw.lua' /home/scripts/draw.lua")
