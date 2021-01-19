16c253e6b53b48481440a2024a73821745aaa29a
jack_young <jackjordanyoung@gmail.com>
2021-01-19 18:55:27 +0000

    First ingame try

---
+++ sprite.lua
@@ -0,0 +1,90 @@
+function idatString(table, start)
+  out = ""
+  for i, dec in pairs(table) do
+    if (i >= start) then
+      out = out .. string.char(dec)
+      --print(string.char(dec))
+    end
+  end
+  return out
+end
+
+function findDecSequence(table, seq)
+  local cur_seq = seq
+  for i, dec in pairs(table) do
+    if (table[i] == string.byte(cur_seq)) then
+      cur_seq = cur_seq:sub(2)
+    elseif (cur_seq == "") then
+      return i
+    elseif not(cur_seq == seq) then
+      cur_seq = seq
+    end
+  end
+end
+
+function fourByteToInt(table, pos)
+  return table[pos]*256^3 + table[pos + 1]*256^2 + table[pos + 2]*256^1 + table[pos + 3]*1
+end
+
+function fileToDecTable(file)
+  local table = {}
+  local n = 0
+  local i = 1
+  local end_c = 0
+  while not(n == null) do
+    n = string.byte(file:read(1))
+    if (end_c == 0) and (n == 73) then end_c = 1
+    elseif (end_c == 1) and (n == 69) then end_c = 2
+    elseif (end_c == 2) and (n == 78) then end_c = 3
+    elseif (end_c == 3) and (n == 68) then end_c = 4
+    else end_c = 0 end
+    --print(n)
+    if (end_c == 4) then n = null
+    else
+      table[i] = n
+      i = i + 1
+    end
+  end
+  return table
+end
+
+local image = assert(io.open("C:\\Users\\jackj\\Documents\\Repos\\test_image.png", "rb"))
+--a = image:read("*a")
+local t = fileToDecTable(image)
+print(t[17])
+print("Width: " .. fourByteToInt(t, 17))
+print("Height: " .. fourByteToInt(t, 21))
+print(findDecSequence(t, "IDAT"))
+local n = findDecSequence(t, "IDAT")
+local s = idatString(t, n)
+
+local component = require("component")
+local data = component.data
+
+s = data.deflate(s)
+
+for i = 0, s:len(), 1 do
+  print(s:sub(i,i))
+end
+
+--[[
+print(string.byte(image:read(1)))
+print(string.byte(image:read(1)))
+print(string.byte(image:read(1)))
+print(string.byte(image:read(1)))
+print(string.byte(image:read(1)))
+print(string.byte(image:read(1)))
+print(string.byte(image:read(1)))
+print(string.byte(image:read(1)))
+print(string.byte(image:read(1)))
+print(string.byte(image:read(1)))
+print(string.byte(image:read(1)))
+print(string.byte(image:read(1)))
+print(string.byte(image:read(1)))
+print(string.byte(image:read(1)))
+print(string.byte(image:read(1)))
+print(string.byte(image:read(1)))
+print(string.byte(image:read(1)))
+print(string.byte(image:read(1)))
+print(image:read("*all"))
+--print(byteRead(image, 1))]]--
